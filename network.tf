4a87b95efc4880e27f2ba639682aef0268da6dfa
Hispanico <marco.giuntini@gmail.com>
2022-04-19 11:13:09 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/hispanico/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "Gaurav"
+  resource_group_name  = azurerm_resource_group.myresourcegroup.name
 }
